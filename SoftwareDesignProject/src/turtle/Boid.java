479687e9b3bc01d84d862b0b9715f823fb0a7bfb
js3952 <js3952@elclas305.its.york.ac.uk>
2025-05-04 13:57:34 +0100

    added boidDistance() method

--- SoftwareDesignProject/src/turtle/Boid.java
+++ SoftwareDesignProject/src/turtle/Boid.java
@@ -26,6 +26,25 @@ public class Boid extends Turtle {
 	public Boid(Canvas canvas, double x, double y, Color color) {
 		super(canvas, new CartesianCoordinate(x,y), color);
 	}
+	/**
+	 * Finds the shortest distance between two boids on a toroidal canvas, prevents boids getting stuck at the edges
+	 * 
+	 * @param boid
+	 * @param xWindowSize
+	 * @param yWindowSize
+	 * @return distance between this and another boid, on a torus with dimensions xWindowSize and yWindowSize
+	 */
+	public double boidDistance(Boid boid, int xWindowSize, int yWindowSize) {
+		double xDistance = Math.abs(this.getPositionX() - boid.getPositionX());
+		double yDistance = Math.abs(this.getPositionY() - boid.getPositionY());
+		if (xDistance > xWindowSize/2) {
+			xDistance = xWindowSize - xDistance;
+		}
+		if (yDistance > yWindowSize/2) {
+			yDistance = yWindowSize - yDistance;
+		}
+		return Math.hypot(xDistance, yDistance);
+	}
 
 	/**
 	 * Moves this boid forward based on time elapsed and speed parameters
