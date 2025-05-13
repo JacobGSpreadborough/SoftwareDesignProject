456d0ee956674ed04629e3eeb522a85ed38148c8
js3952 <js3952@elclas305.its.york.ac.uk>
2025-05-13 21:13:45 +0100

    Added obstacle class

---
+++ SoftwareDesignProject/src/obstacle/Obstacle.java
@@ -0,0 +1,69 @@
+package obstacle;
+
+import java.awt.Color;
+
+import drawing.Canvas;
+import geometry.CartesianCoordinate;
+
+public class Obstacle {
+
+	private Canvas canvas;
+	private Color color;
+	private final CartesianCoordinate topLeftCorner;
+	private final CartesianCoordinate topRightCorner;
+	private final CartesianCoordinate bottomLeftCorner;
+	private final CartesianCoordinate bottomRightCorner;
+	private final int xLength;
+	private final int yLength;
+	private final int area;
+	
+	public Obstacle(Canvas canvas, CartesianCoordinate topLeftCorner,int xLength,int yLength, Color color) {
+		this.canvas = canvas;
+		this.color = color;
+		this.topLeftCorner = topLeftCorner;
+		this.xLength = xLength;
+		this.yLength = yLength;
+		this.area = xLength * yLength;
+		topRightCorner = new CartesianCoordinate(topLeftCorner.getX() + xLength, topLeftCorner.getY());
+		bottomRightCorner = new CartesianCoordinate(topLeftCorner.getX() + xLength, topLeftCorner.getY() + yLength);
+		bottomLeftCorner = new CartesianCoordinate(topLeftCorner.getX(), topLeftCorner.getY() + yLength);
+		draw();
+	}
+
+	/**
+	 * draws the obstacle
+	 */
+	public void draw() {
+		canvas.drawLineBetweenPoints(topLeftCorner, topRightCorner, color);
+		canvas.drawLineBetweenPoints(topRightCorner, bottomRightCorner, color);
+		canvas.drawLineBetweenPoints(bottomRightCorner, bottomLeftCorner, color);
+		canvas.drawLineBetweenPoints(bottomLeftCorner, topLeftCorner, color);
+	}
+
+	/**
+	 * erases the obstacle from the canvas
+	 */
+	public void unDraw() {
+		canvas.removeMostRecentLine();
+		canvas.removeMostRecentLine();
+		canvas.removeMostRecentLine();
+		canvas.removeMostRecentLine();
+	}
+	
+	public int getxLength() {
+		return xLength;
+	}
+
+	public int getyLength() {
+		return yLength;
+	}
+
+	public int getArea() {
+		return area;
+	}
+
+	public CartesianCoordinate getTopLeftCorner() {
+		return topLeftCorner;
+	}
+
+}
