92c74d30599667416261e19cf1e6dc78028d6315
js3952 <js3952@elclas310.its.york.ac.uk>
2025-05-26 21:23:44 +0100

    started predator class

---
+++ SoftwareDesignProject/src/turtle/Predator.java
@@ -0,0 +1,22 @@
+package turtle;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.util.List;
+
+import drawing.Canvas;
+import geometry.CartesianCoordinate;
+import simulationObject.SimulationObject;
+
+public class Predator extends Boid implements SimulationObject{
+	
+	public Predator(Canvas canvas, CartesianCoordinate startingPoint, Dimension canvasSize, Color color) {
+		super(canvas, startingPoint, canvasSize, color);
+	}
+	
+	public void chaseBoids(List<Boid> flock, double hunting) {
+		double angleToTurn = currentPosition.angle(nearestBoid(flock).currentPosition);
+		turn((int)(angleToTurn * hunting));
+	}
+	
+}
